a244e5b3a148bd1a088fc9b5be3803c0d9f0fe1f
pbharath0428 <113876204+pbharath0428@users.noreply.github.com>
2022-09-20 10:27:46 +0530

    Add files via upload

---
+++ palindrome.(4).py
@@ -0,0 +1,12 @@
+def ispalindrome(s) :
+    rev =''.join(reversed(s))
+    if (s==rev):
+        return True
+    return False
+s=input("enter the word")
+ans= ispalindrome(s)
+if(ans):
+    print("yes")
+else:
+    print("no")
+
